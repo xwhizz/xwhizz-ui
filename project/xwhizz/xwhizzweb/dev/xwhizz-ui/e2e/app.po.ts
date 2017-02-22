1868780ff410c6256dc592a5cb2c7fc756816987
xwhizz <xwhizztech@gmail.com>
2017-02-21 23:34:33 -0500

    new files added by vipal
    new files added by vipal

---
+++ project/xwhizz/xwhizzweb/dev/xwhizz-ui/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class XWhizzUiPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}

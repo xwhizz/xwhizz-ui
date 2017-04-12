5e94669b6f1f7c680cf991ebedf8bb0d4ac0faf8
Sandip <sandip1805@gmail.com>
2017-04-12 13:04:56 +0530

    Code updated for contactus page

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { ContactUs }    from './contact-us';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+
+  model = new ContactUs('', '', '', '', '');
+  contactUsUrl = '/api/v1/contactUs';
+
+  constructor(private http: Http) {
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.model);
+    let bodyString = JSON.stringify(this.model); // Stringify payload
+    console.log(bodyString);
+    let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+    let options = new RequestOptions({ headers: headers }); // Create a request option
+
+    return this.http.post(this.contactUsUrl, bodyString, options) // ...using post request
+                     .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+                     .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+
   }
 
 }
---
+++ src/app/contact-us/contact-us.ts
@@ -0,0 +1,9 @@
+export class ContactUs {
+  constructor(
+    public name: string,
+    public address: string,
+    public email: string,
+    public telephone: string,
+    public notes: string
+  ) {  }
+}

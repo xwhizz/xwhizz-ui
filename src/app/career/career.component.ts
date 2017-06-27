9f08b95bf58710671bb680c9d64a93ff7f2ed980
Sandip <sandip1805@gmail.com>
2017-06-27 12:00:44 +0530

    code updated for career page

--- src/app/career/career.component.ts
+++ src/app/career/career.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CareerComponent implements OnInit {
 
+  tabs = ['Career and Development', 'Innovative Technology stack', 'Freedom to learn new technology',
+      'Work Life Balance', 'Reward and Recognition', 'Value and Leadership'];
+
+  selectedTab = 0;
+
+  changeSelectedTab(tab) {
+    this.selectedTab = tab;
+  }
+
   constructor() { }
 
   ngOnInit() {

613815fbcb97c58d269800941e688c9ca4c9a542
Sandip <sandip1805@gmail.com>
2017-12-05 22:50:42 +0530

    Project code updated for latest design

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-home-page',
@@ -10,6 +11,23 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('#loader').delay(1000).fadeOut('slow');
+    $('.slider-active').owlCarousel({
+      autoPlay: 800000000,
+      animateOut: 'fadeOut',
+      animateIn: 'fadeIn',
+      items: 1,
+      dots: true,
+      slideSpeed: 500,
+      pagination: true,
+      navigation: false,
+      loop: true,
+      mouseDrag: true,
+      singleItem: true,
+      transitionStyle: "fade",
+      addClassActive: true,
+    });
   }
+
 
 }
